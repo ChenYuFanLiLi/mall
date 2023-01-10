c3957a298c1eab2163a379bad290500c2eff0670
太妃糖的诱惑 <50236315+ChenYuFanLiLi@users.noreply.github.com>
2023-01-11 00:39:46 +0800

    初始提交

---
+++ src/main/java/com/example/mallsrpingboot/MallSrpingbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallsrpingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSrpingbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSrpingbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mallsrpingboot/MallSrpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallsrpingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSrpingbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
